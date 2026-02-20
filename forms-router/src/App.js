0ad8f03f7feb3eb6f78768c629d863ed318747d0
TalalJutt <talaljuttarifhussain@gmail.com>
2026-02-20 02:31:13 -0800

    Add files via upload

---
+++ forms-router/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import "bootstrap/dist/js/bootstrap.bundle"
+import AddProducts from './pages/AddProducts';
+
+
+function App() {
+return (
+  <>
+  <AddProducts />
+  </>
+);
+}
+export default App;
